110078bf37dbbe07ea04b420d3ff47a29b692168
kis0421 <kig0421@gmail.com>
2021-12-16 23:16:20 +0900

    최초

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom"
+
+const App = () => {
+    return <h1>first</h1>
+}
+ReactDOM.render(<App />, document.getElementById("app"));

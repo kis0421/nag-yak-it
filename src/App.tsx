db84c60774d195f282675319c646f95812ff217b
kis0421 <kig0421@gmail.com>
2022-01-25 01:46:43 +0900

    apollo/client 사용 가능하게

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import * as React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+const secretKey = process?.env?.REACT_APP_SECRETKEY || "";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://study-diary.hasura.app/v1/graphql",
+    headers: {
+      "x-hasura-admin-secret": secretKey
+    },
+  }),
+  cache: new InMemoryCache()
+});
 
 const App = () => {
   return <h1>
     ㅎㅇ
   </h1>
 }
-export default App;
+
+export default () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
